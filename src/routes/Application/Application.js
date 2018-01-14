ffb051fa143f40603251de9c52852c8d5277996e
n313893254 <n313893254@126.com>
2018-01-14 19:35:08 +0800

    Add user manager modal

--- src/routes/Application/Application.js
+++ src/routes/Application/Application.js
@@ -1,65 +1,28 @@
-import { Table, Icon, Pagination, Tag } from 'antd';
+import { Table, Icon, Pagination, Tag, Modal, Form, Input, Button } from 'antd';
 import React from 'react'
 import { Link } from 'react-router-dom'
+import  ActionDropdown from '../../components/ActionDropdown'
 
 import { connect as modelConnect } from 'utils/ecos'
 
-const columns = [{
-  title: '序号',
-  dataIndex: 'key',
-}, {
-  title: '应用名称',
-  dataIndex: 'name',
-  sorter: (a, b) => a.name.length - b.name.length,
-}, {
-  title: '项目经理',
-  dataIndex: 'manager',
-  defaultSortOrder: 'descend',
-  sorter: (a, b) => a.manager.length - b.manager.length,
-}, {
-  title: '应用告警数',
-  dataIndex: 'health',
-}, {
-  title: '资源占用率',
-  dataIndex: 'resourceUsage',
-}, {
-  title: '资源种类',
-  dataIndex: 'resourceUsage',
-  render() {
-    return (
-      <div>
-        <Tag>M</Tag>
-        <Tag>P</Tag>
-        <Tag>R</Tag>
-        <Tag>MQ</Tag>
-      </div>
-    )
+const FormItem = Form.Item
+const {Search} = Input
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 5 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 12 },
+  },
+  style: {
+    marginBottom: '10px'
   }
-},
-// {
-//   title: '中间件健康度',
-//   dataIndex: 'middleWareHealth',
-//   render(text, record, index){
-//     return (
-//         <Icon type="database">{text}</Icon>
-//       )
-//   }
-// },
-{
-  title: '状态',
-  dataIndex: 'status',
-}, {
-  title: '操作',
-  dataIndex: 'actions',
-  render(text, record, index){
-    return (
-      <div>
-        <Link to={`/resourcesRequest`}>申请资源</Link>
-        <Link className="mg-l10" to={`/applications/${record.key}`}>{text}</Link>
-      </div>
-    )
-  }
-}];
+};
+
+
 
 const data = [{
   key: '1',
@@ -77,7 +40,83 @@ function onChange(pagination, filters, sorter) {
 }
 
 class Application extends React.Component {
+  state = { visibleAdd: false, visibleEdit: false, visibleDetail: false, }
+
+  showModal = (visible) => {
+    return ()=>{this.setState({
+      [visible]: true,
+    });
+  }
+  }
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visibleAdd: false,
+      visibleEdit: false,
+      visibleDetail: false,
+    });
+  }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visibleAdd: false,
+      visibleEdit: false,
+      visibleDetail: false,
+    });
+  }
+
   render(){
+    const columns = [{
+      title: '序号',
+      dataIndex: 'key',
+    }, {
+      title: '应用名称',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.length - b.name.length,
+    }, {
+      title: '项目经理',
+      dataIndex: 'manager',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.manager.length - b.manager.length,
+    }, {
+      title: '应用告警数',
+      dataIndex: 'health',
+    }, {
+      title: '资源占用率',
+      dataIndex: 'resourceUsage1',
+    }, {
+      title: '资源种类',
+      dataIndex: 'resourceUsage',
+      render() {
+        return (
+          <div>
+            <Tag>M</Tag>
+            <Tag>P</Tag>
+            <Tag>R</Tag>
+            <Tag>MQ</Tag>
+          </div>
+        )
+      }
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+    }, {
+      title: '操作',
+      dataIndex: 'actions',
+      render: (text, record, index) => {
+        return (
+          <div>
+            <Link to={`/resourcesRequest`}>申请资源</Link>
+            <Link className="mg-l10" to={`/applications/${record.key}`}>{text}</Link>
+            <a className="mg-l10"
+               onClick={this.showModal('visibleEdit')}
+            >成员管理</a>
+          </div>
+        )
+      }
+    }];
     return (
     <div>
       <div className="page-section">
@@ -85,7 +124,48 @@ class Application extends React.Component {
       </div>
       <section className="page-section bottom-actions text-center">
           <Pagination showQuickJumper defaultCurrent={2} total={500} onChange={this.onChange} />
-        </section>
+      </section>
+      <Modal
+        title="成员管理"
+        visible={this.state.visibleEdit}
+        okText="添加"
+        cancelText="取消"
+        onOk={this.handleOk}
+        onCancel={this.handleCancel}
+      >
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem
+            {...formItemLayout}
+            label="当前应用"
+          >
+            大数据
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="成员"
+          >
+            <p>张三  12123124231<Icon className="mg-l10" type="delete" onClick={e => console.log('poi')} /></p>
+            <p>张三  12123124231<Icon className="mg-l10" type="delete" onClick={e => console.log('poi')} /></p>
+            <p>张三  12123124231<Icon  className="mg-l10" type="delete" onClick={e => console.log('poi')} /></p>
+          </FormItem>
+        </Form>
+        <h3>查询</h3>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem
+            {...formItemLayout}
+            label="账号"
+          >
+
+              <Search />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="用户信息"
+          >
+            <p>张三  12123124231</p>
+          </FormItem>
+        </Form>
+      </Modal>
     </div>
       )
   }
