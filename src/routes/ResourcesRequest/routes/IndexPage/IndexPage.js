a5f54484bdf3d868bd5620afae2e1deeeb174400
n313893254 <n313893254@126.com>
2018-01-17 11:01:46 +0800

    Implemented Approval page

--- src/routes/ResourcesRequest/routes/IndexPage/IndexPage.js
+++ src/routes/ResourcesRequest/routes/IndexPage/IndexPage.js
@@ -1,58 +1,98 @@
 import React from 'react'
 import {Table, Row, Col, Input, Button, Select} from 'antd'
+import Detail from './Detail'
 
 const {Search} = Input
 const {Option} = Select
 
 import styles from './styles.scss'
 
+const datas =[{
+  id: 1,
+  type: '中间件',
+  applicant: '张三',
+  department: '众创汇',
+  time: '20170901',
+  project: '海尔690大数据项目',
+  state: '已驳回',
+  action: '查看',
+}, {
+  id: 2,
+  type: 'paas',
+  applicant: '李四',
+  department: '众创汇',
+  time: '20170901',
+  project: '海尔690大数据项目',
+  state: '待审批',
+  action: '查看',
+}]
+
 class C extends React.Component {
+  state = {
+    filteredInfo: null,
+    visibleDetail: false,
+    record: {},
+  };
+
+  handleCancel = (e) => {
+    this.setState({
+      visibleDetail: false,
+    });
+  }
+
   render() {
+    let { filteredInfo, } = this.state;
     const columns = [{
       title: '序号',
-      dataIndex: 'state',
-      key: 'state',
-      render: (state, record) => {
-        return (
-          <div>
-            <Tag {...getState(record.state)}>
-              {nameMap[state]}
-            </Tag>
-          </div>
-        )
-      }
+      dataIndex: 'id',
+      key: 'id',
     }, {
       title: '类型',
-      dataIndex: 'description',
-      key: 'description',
+      dataIndex: 'type',
+      key: 'type',
+      filters: [
+        { text: '全部', value: 'all' },
+        { text: '中间件', value: '中间件' },
+        { text: 'PAAS', value: 'paas' },
+        { text: '能力开放平台', value: 'Platform' },
+      ],
+      onFilter: (value, record) => {
+        if (value === 'all') {
+          return record.type
+        }
+        return record.type.includes(value)
+      },
+      filterMultiple: false,
     }, {
       title: '申请人',
-      dataIndex: 'rule',
-      key: 'rule',
+      dataIndex: 'applicant',
+      key: 'applicant',
+    }, {
+      title: '部门',
+      dataIndex: 'department',
+      key: 'department',
     }, {
       title: '申请时间',
-      dataIndex: 'endPoint',
-      key: 'endPoint',
+      dataIndex: 'time',
+      key: 'time',
     }, {
-      title: '应用名称',
-      dataIndex: 'startsAt',
-      key: 'startsAt',
-      render: (startsAt, record) => {
-        return <span>{ (record.state === 'active' || record.state === 'suppressed') && startsAt}</span>
-      }
+      title: '项目名称',
+      dataIndex: 'project',
+      key: 'project',
     }, {
       title: '状态',
-      dataIndex: 'endsAt1',
-      key: 'endsAt1',
-      render: (endsAt, record) => {
-        return <span>{ (record.state === 'active' || record.state === 'suppressed') && endsAt}</span>
-      }
+      dataIndex: 'state',
+      key: 'state',
     }, {
       title: '操作',
-      dataIndex: 'endsAt',
-      key: 'endsAt',
-      render: (endsAt, record) => {
-        return <span>{ (record.state === 'active' || record.state === 'suppressed') && endsAt}</span>
+      dataIndex: 'action',
+      key: 'action',
+      render: (action, record) => {
+        return (
+          <div>
+            <a onClick={e => this.setState({visibleDetail: true, record})}>{action}</a>
+          </div>
+        )
       }
     },];
     return (
@@ -70,12 +110,18 @@ class C extends React.Component {
         <Row>
         <Col>
           <Table
-            // dataSource={boxes}
+            dataSource={datas}
             columns={columns}
             rowKey="id"
           />
         </Col>
       </Row>
+      <Detail
+        visible={this.state.visibleDetail}
+        onOk={(newData) => {this.saveAdd(newData)}}
+        onCancel={this.handleCancel}
+        resource={this.state.record}
+        />
     </main>
     )
   }
