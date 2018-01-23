ad93e65c9717770fe4f8c7ebcb443f5b1987557d
n313893254 <n313893254@126.com>
2018-01-23 10:07:16 +0800

    Add state nameMap

--- src/utils/nameMap.js
+++ src/utils/nameMap.js
@@ -1,6 +1,24 @@
 export default {
-  // Approval
+  // state
+  active: '可用',
+  inactive: '停用',
+  activating: '启用中',
+  deactivating: '停用中',
+  creating: '创建中',
+  updating: '更新中',
+  removing: '移除中',
+  removed: '已移除',
   pendding: '待审批',
+  rejected: '已驳回',
+  ApplyingResource: '待部署',
+  ResourceReady: '已部署',
+  DeployingResource: '正在部署',
+  Updating: '更新中',
+  deploying: '部署中',
+  ready: '就绪',
+  deployError: '失败',
+  confirmed: '已通过',
+  denied: '已驳回',
 
   // Account
   internal: '内部员工',
@@ -10,8 +28,7 @@ export default {
   domainAdmin: '团队长',
   admin: '管理员',
 
-  active: '可用',
-  inactive: '停用',
+
 
   // Project
   ApplyingResource: '待部署',
